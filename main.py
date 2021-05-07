a87f788212cb4b674f523f27d873931ef6b62485
KienNguyen <>
2021-05-07 17:45:20 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,48 @@
+#Calculator
+
+#Add :
+def add(n1, n2) :
+  return n1 + n2
+
+#Subtract :
+
+def subtract(n1, n2) :
+  return n1 - n2
+
+#Multiply :
+
+def multiply(n1, n2) :
+  return n1 * n2
+
+# Divide :
+def divide(n1, n2) :
+  return n1 / n2  
+
+#Dictionary in Function :
+
+operations = {
+  "+ " : add,
+  "-"  : subtract,
+  "*"  : multiply,
+  "/"  :  divide,
+}
+#function = operations ["+"]
+#function (2,3)
+
+num1 = int(input(" What is the first number :? "))
+num2 = int(input(" What is the second number :? "))
+
+# Use For Loop to get through each symbol in the dictionary :
+
+for symbol in operations :
+  print(symbol)
+
+operation_symbol = input("Pick an operation from the line above : ")  
+
+calculation_function = operations [operation_symbol]
+
+answer = calculation_function (num1, num2)
+
+
+
+print(f"{num1}{operation_symbol}{num2} ={answer}")
